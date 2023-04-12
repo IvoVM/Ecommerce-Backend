4685e392686e916d4ea9c6863874029572b115d0
IvoVM <ivomastrangelo12@gmail.com>
2023-04-12 02:50:43 -0300

    Created cards in existance array

---
+++ src/routes/utils/cards.js
@@ -0,0 +1,43 @@
+const starterproducts = [
+    {
+      _id: "1",
+      description: "1 Litro C/manija 24hs Frio/calor",
+      title: "Termo Stanley Classic",
+      price: "240",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0IsmaUJqr3tuwQNkhmNE_o87RkiGlt4XQmA&usqp=CAU",
+      quantity: 1,
+      undeleteable: true,
+    },
+    {
+      _id: "2",
+      description: "Tazas con frase",
+      title: "Tazas Frases",
+      price: "10",
+      img: "https://duniapol.com.ar/wp-content/uploads/2020/11/2-600x600.jpg",
+      quantity: 1,
+      total: "200",
+      undeleteable: true,
+    },
+    {
+      _id: "3",
+      description: "oreo cookies",
+      title: "oreo",
+      price: "5",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlttdd84Q6WvEpH24FMfoQ8Y2Rg3vKn-hKGw&usqp=CAU",
+      quantity: 1,
+      total: "200",
+      undeleteable: true,
+    },
+    {
+      _id: "4",
+      description: "coca cola,coca zero or coca cola ligth",
+      title: "coca cola",
+      price: "20",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo-ICGGrJymnBzC83DUMeYoRbymh5SqJieHg&usqp=CAU",
+      quantity: 1,
+      total: "200",
+      undeleteable: true,
+    },
+  ];
+  export default starterproducts;
+  
