976874df6e7b4f3804d505cc8f44c33635569e9c
IvoVM <ivomastrangelo12@gmail.com>
2023-04-14 22:19:33 -0300

    Get product by id
    Implemented function to get product by id, also added try and catch to the functions

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,26 +4,55 @@ import Product from "../models/Product";
 const router = Router();
 
 router.get("/products", async (req, res) => {
-  let products = await Product.find();
-  products = products.concat(cardsinexistance);
-  res.status(200).send(products);
+  try {
+    let products = await Product.find();
+    products = products.concat(cardsinexistance);
+    res.status(200).send(products);
+  } catch (err) {
+    console.log(err);
+  }
 });
 router.get("/products/:id", async (req, res) => {
-  let product = await Product.find({ _id: req.params.id });
-  res.status(200).send(product);
+  try {
+    let id = req.params.id;
+    if (id.length === 1) {
+      cardsinexistance.map((product) => {
+        if (product._id === id) {
+          return res.status(200).send({ msg: "product found", product });
+        }
+      });
+    } else {
+      let product = await Product.findById(id);
+      return res.status(200).send({ msg: "product found", product });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+router.delete("/products/:id", async (req, res) => {
+  try {
+    await Product.deleteOne({ _id: req.params.id });
+    res.status(200).send("Deleted successfully");
+  } catch (e) {
+    console.log(e);
+  }
 });
 router.post("/products/upload", async (req, res) => {
-  let { title, price, description, img, categorie } = req.body;
-
-  let product = new Product({
-    title,
-    price,
-    description,
-    img,
-    categorie,
-    quantity: 1,
-  });
-  await product.save();
+  try {
+    let { title, price, description, img, categorie } = req.body;
+    let product = new Product({
+      title,
+      price,
+      description,
+      img,
+      categorie,
+      quantity: 1,
+    });
+    await product.save();
+    res.status(200).send("Product saved");
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 export default router;
