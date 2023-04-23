58c02e8206fb591ccad3221b16f5999a826cf3f6
IvoVM <ivomastrangelo12@gmail.com>
2023-04-23 14:49:58 -0300

    Added route to get this user's  products

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,6 +5,7 @@ import {
   getProductsByCategory,
   createProduct,
   deleteProductById,
+  getUserProducts
 } from "../controllers/productsController";
 import { verifyToken } from "../libs/verifyToken";
 
@@ -14,9 +15,11 @@ router.get("/products", getProducts);
 
 router.get("/products/:id", getProductById);
 
-router.get("/products/category=:category", getProductsByCategory);
+router.get("/category/:category", getProductsByCategory);
 
 router.post("/products/upload", verifyToken, createProduct);
+
+// router.get("/products/profile", verifyToken, getUserProducts);
 
 router.delete("/products/:id", verifyToken, deleteProductById);
 
